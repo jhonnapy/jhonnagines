a5c7c7519ddef5616ea1d344e8ebe3763d7c4b51
paolo julian <paolojulian.personal@gmail.com>
2023-07-25 13:50:06 +0800

    feat: Initialize

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react';
+
+export type PortfolioPageProps = {
+    // No Props
+};
+
+const PortfolioPage: FunctionComponent<PortfolioPageProps> = (props) => {
+  return (
+    <div className=""></div>
+  );
+}
+
+export default PortfolioPage;
