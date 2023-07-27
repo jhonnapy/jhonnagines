ad59f8125390fb69152afeffb63961134fd4c9bf
paolo julian <paolojulian.personal@gmail.com>
2023-07-28 00:20:47 +0800

    feat: Add bloglist UI

--- src/app/blogs/_components/blog-item.tsx
+++ src/app/blogs/_components/blog-item.tsx
@@ -1,3 +1,7 @@
+import Row from '@/components/layouts/row';
+import Stack from '@/components/layouts/stack';
+import classNames from 'classnames';
+import Link from 'next/link';
 import React, { FunctionComponent } from 'react';
 
 export type BlogItemProps = {
@@ -5,7 +9,39 @@ export type BlogItemProps = {
 };
 
 const BlogItem: FunctionComponent<BlogItemProps> = (props) => {
-  return <div className=''></div>;
+  return (
+    <Link href='/blogs/lorem-ipsum'>
+      <Row className='items-center group'>
+        <div className='relative w-[150px] h-[150px] bg-slate-50 border-b border-l border-r border-slate-400'>
+          <div className='absolute border-t border-slate-400 top-0 -left-4 w-[200%]'></div>
+        </div>
+
+        <Stack className='flex-1 h-full p-4 relative'>
+          <h4 className='font-semibold text-slate-800'>
+            Lorem Ipsum - King of all Kings
+          </h4>
+
+          <p className='line-clamp-2 text-sm text-slate-500'>
+            Et reprehenderit cupidatat consectetur occaecat ea aliqua non
+            aliquip labore proident exercitation pariatur duis sint. In ea aute
+            voluptate et mollit et. Proident minim velit amet tempor velit et
+            amet officia sit aliquip qui nostrud. Proident adipisicing aliquip
+            sit nisi id veniam consequat dolore commodo.
+          </p>
+          <span className='mt-2 font-medium text-sm text-slate-500'>
+            learn more...
+          </span>
+
+          <div
+            className={classNames(
+              'absolute bottom-0 right-0 h-4/5 w-1/2 border-r border-b border-slate-400',
+              'transition'
+            )}
+          ></div>
+        </Stack>
+      </Row>
+    </Link>
+  );
 };
 
 export default BlogItem;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,4 +1,7 @@
 import React, { FunctionComponent } from 'react';
+import BlogItem from './_components/blog-item';
+import Stack from '@/components/layouts/stack';
+import Row from '@/components/layouts/row';
 
 export type BlogsPageProps = {
   // No Props
@@ -8,7 +11,24 @@ const BlogsPage: FunctionComponent<BlogsPageProps> = (props) => {
   return (
     <>
       <div className='p-12'>
-        <h1>view my latest blogs</h1>
+        <Stack className='space-y-8'>
+          <Row className='justify-between'>
+            <h1 className='font-semibold text-slate-600'>
+              view my latest blogs
+            </h1>
+            <input
+              className='p-2 border-b border-slate-400 bg-inherit w-fit focus:outline-none focus:border-blue-400'
+              placeholder='search'
+            />
+          </Row>
+          <Stack className='space-y-6'>
+            <BlogItem />
+            <BlogItem />
+            <BlogItem />
+            <BlogItem />
+            <BlogItem />
+          </Stack>
+        </Stack>
       </div>
     </>
   );
